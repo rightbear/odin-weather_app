e30dfa52a67b52c3379b336564ab996bc83707c5
rightbear <jjj17041113@gmail.com>
2025-07-01 14:33:21 +0800

    feat: add functions that process the JSON and return an object with data required

--- src/function/APIControl.js
+++ src/function/APIControl.js
@@ -2,21 +2,40 @@ export async function retrieveData(myKey){
     let location = null;
     
     while(!location) {
-        location = prompt("Input a City or US Zip Code");
+        location = prompt("Search a city..");
     }
     
     try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${myKey}`, { mode: "cors" });
-
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next5days?unitGroup=metric&key=${myKey}`, { mode: "cors" });
+        
         if(response.status === 400) {
-            throw ("Error location parameter");
+            // Status code 400 means the format of the API is incorrect or an invalid parameter for location
+
+            // TODO: Show error message below the input field
+
+            throw new Error(`Error parameter for the location: ${location}`);
+        }
+        else if(!response.ok) {
+            // response.ok is true when status code is 200~299
+
+            alert("Oops...Some errors just happen! Maybe refresh the page or search again later.")
+            
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const weatherData = await response.json();
+
+        // Use object destructing to get location, date, time, temperature, condition, condition-icon, temp-max, temp-min, temp-feelslike, humidity, windspeed, uvindex, future-5-days-data
+        const { resolvedAddress: locationAddress, currentConditions: {datetime: currentTime, temp, conditions, icon, feelslike, humidity, windspeed, uvindex},
+                days: [ {datetime: currentDate, tempmax: todaytempMax, tempmin: todaytempMin}, ...future5Days] } = weatherData;
+
+        const selectedFieldsObject =  { locationAddress, currentDate, currentTime, temp, conditions, icon, todaytempMax, todaytempMin, feelslike, humidity, windspeed, uvindex, future5Days };
+
+        console.log(selectedFieldsObject);
         console.log(weatherData);
     }
     catch (error) {
-      // If there is some error thrown out when requesting the GIF, show the error message
+      // If there is some error thrown out when requesting the weather data, show the error message
       console.log(error);
     }
 }
